c9bef54171d219842976478765c0c505235d0886
woussoren <woussoren@gmail.com>
2019-05-10 17:50:27 +0200

    Added basic routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing.module';
 import {SlideMenuModule} from 'primeng/slidemenu';
 import {ButtonModule} from 'primeng/button';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, SlideMenuModule, ButtonModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, SlideMenuModule, AppRoutingModule, ButtonModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,26 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { NavigationComponent} from './navigation/navigation.component';
+import { FormsModule } from '@angular/forms';
+import {SlideMenuModule} from 'primeng/slidemenu';
+import {ButtonModule} from 'primeng/button';
+
+@NgModule({
+  declarations: [ 
+    NavigationComponent
+  ],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'navigation/:afdeling', component: NavigationComponent },
+      { path: '**', redirectTo: 'navigation' }
+    ]),
+    FormsModule, SlideMenuModule, ButtonModule
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [],
+
+})
+export class AppRoutingModule {}
